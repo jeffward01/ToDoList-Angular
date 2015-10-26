438f37cb095286992d3e22bfc79bf13cec4883a2
jeffward01 <jeffwardcoding@gmail.com>
2015-10-26 12:45:28 -0700

    working V.1

---
+++ ToDo/ToDo.Client/js/ToDo.ctrl.js
@@ -0,0 +1,16 @@
+﻿angular.module('ToDoApp').controller('ToDoController', function ($scope) {
+
+    $scope.todos = [];
+
+    $scope.newToDo = '';
+
+    //Add's To Do to list
+    $scope.AddToDo = function () {
+        $scope.todos.push($scope.newToDo);
+
+        $scope.newToDo = '';
+    };
+
+
+
+});
